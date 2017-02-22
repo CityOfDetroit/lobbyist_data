32e2493b1c8de479ad2c1339d974889a57544af1
karlkaebnick <karl.kaebick@gmail.com>
2017-02-22 14:15:47 -0500

    Stub out the api

--- lobbyist_data/settings.py
+++ lobbyist_data/settings.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lookup',
-    # 'rest_framework',
+    'rest_framework',
     # 'mod_wsgi.server',
 )
 
--- lookup/views.py
+++ lookup/views.py
@@ -1,5 +1,21 @@
-from django.http import HttpResponse
+import json
+
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# from .serializers import ScheduleExceptionSerializer
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the lookup index.")
+
+@api_view(['GET'])
+def lookup(request, format=None):
+    """
+    List lobbyist data
+    """
+    if request.method == 'GET':
+        content = { "foo": "bar" }
+        return Response(content)
